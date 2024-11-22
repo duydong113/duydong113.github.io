e8bbab0d4c12f7a5e9a8b642ab848ddc2b49a49b
duydong113 <154010197+duydong113@users.noreply.github.com>
2024-11-22 12:21:28 +0700

    tuan 8

--- Week8/script.js
+++ Week8/script.js
@@ -1,10 +1,13 @@
+
 function calculate(operator) {
     const a_value = parseFloat(document.getElementById('a_value').value);
     const b_value = parseFloat(document.getElementById('b_value').value);
     const errorDiv = document.getElementById('error');
     const resultDiv = document.getElementById('result');
+
     errorDiv.textContent = '';
     resultDiv.textContent = '';
+
     if (isNaN(a_value) || isNaN(b_value)) {
         errorDiv.textContent = 'Điền lại số hợp lí';
         return;
@@ -33,5 +36,16 @@ function calculate(operator) {
             return;
     }
 
+    lastResult = result; 
     resultDiv.textContent = `Kết quả: ${result}`;
+}
+
+function check() {
+    const a_value = parseFloat(document.getElementById('a_value').value);
+    const resultDiv = document.getElementById('result');
+    if (a_value % 2 === 0) {
+        resultDiv.textContent = `Số ${a_value} là số chẵn`;
+    } else {
+        resultDiv.textContent = `Số ${a_value} là số lẻ`;
+    }
 }
